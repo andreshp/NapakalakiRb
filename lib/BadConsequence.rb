d4e0ef546db9f4cf223b1f348a944a4b0b37a3cb
jlsuarezdiaz <jlsuarezdiaz@correo.ugr.es>
2015-03-07 17:50:57 +0100

    A second idea of BadConsequence. Not tested

--- lib/BadConsequence.rb
+++ lib/BadConsequence.rb
@@ -26,6 +26,38 @@ module Napakalaki
 		end
 =end
 		
+		def self.new_by_number(text, levels, nVisible, nHidden)
+			@text = text
+			@levels = levels
+			@nVisibleTreasures = nVisible
+			@nHiddenTreasures = nHidden
+
+			@specificVisibleTreasures = nil
+			@specificHiddenTreasures = nil
+		end
+
+		def self.new_by_death(text,death)
+			@text = text
+			@death = death
+
+			@nHiddenTreasures = 0
+			@nVisibleTreasures = 0
+
+			@specificVisibleTreasures = nil
+			@specificHiddenTreasures = nil
+		end
+
+		def self.new_by_specific(text, levels, tVisible, tHidden)
+			@text = text
+			@death = death
+
+			@specificHiddenTreasures = tHidden
+			@specificVisibleTreasures = tVisible
+
+			@nHiddenTreasures = 0
+			@nVisibleTreasures = 0
+		end
+		
 		attr_reader :text :levels :nVisibleTreasures :nHiddenTreasures
 		#esto o getText, getLevels, etc.?
 
