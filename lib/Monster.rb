6d8fb56e89804ac931f69dbb0fc6eefc9ad9a6a5
andreshp <andreshp9@gmail.com>
2015-03-07 19:33:13 +0100

    Monster Class developped.

--- lib/Monster.rb
+++ lib/Monster.rb
@@ -4,5 +4,36 @@
 # Programación Dirigida y Orientada a Objetos - Ruby Napakalaki
 #######################################################################
 
-module Napakalaki
+# Monster Class.
+# This class has all the information related to the monsters
+# in Napakalaki game.
+# 
+# @author andreshp, jlsuarez
+#
+class Monster
+        
+    # Gets name of the monster.
+    attr_reader :name
 
+    # Gets the combat level of the monster.
+    attr_reader :combatLevel
+
+    # Initialize method.
+    # Initializes name, combatlevel, prize, badCons.
+    # @param name Monster's name.
+    # @param level Monster's level.
+    # @param bc Bad consequence.
+    # @param prize Prize.
+    def initialize(name, level, bc, prize)
+        @name = name
+        @combatLevel = level
+        @prize = prize
+        @badCons = bc
+    end
+
+    # Gets a string with the monster's info.
+    # @return string with information.
+    def toString
+       "Name = " + name + "\nLevel = " + combatLevel.to_s + "\nPrize = [" + prize.toString + "] \nBad Consequence = [" + badCons.toString + "]"
+    end
+end
