a677f189cf55b090f57d2d0632588c1f76519eec
jlsuarezdiaz <jlsuarezdiaz@correo.ugr.es>
2015-03-08 01:32:34 +0100

    Added comments to BadConsequence

--- lib/BadConsequence.rb
+++ lib/BadConsequence.rb
@@ -12,30 +12,15 @@
 # @author andreshp, jlsuarez
 class BadConsequence
 
-=begin No puedo poner los tres :(
-    def initialize(text, levels, nVisible, nHidden)
-        @text = text
-        @levels = levels
-        @nVisibleTreasures = nVisible
-        @nHiddenTreasures = nHidden
-    end
-
-    def initialize(text,death)
-        @text = text
-        @death = death
-    end
-
-    @nHiddenTreasures = 0
-        @nVisibleTreasures = 0
-    end
-
-    def initialize(text, levels, tVisible, tHidden)
-        @text = text
-        @levels = levels
-        @specificVisibleTreasures = tVisible
-        @specificHiddenTreasures = tHidden
-    end
-=end
+    # Private constructor whichs initializes all attributes.
+    #
+    # @param text [string] Text that explains what does the bas consequence do. It sets text attribute.
+    # @param levels [int] Levels that are lost with the bad consequence. It sets levels attribute.
+    # @param death [boolean] Value that indicates if the nad consequence is death. It sets death attribute.
+    # @param nVisible [int] Number of Visible Treasures that are lost. It sets nVisibleTreasures attribute.
+    # @param nHidden [int] Number of Hidden Treasures that are lost. It sets nHiddenTreasures attribute.
+    # @param tVisible [int] Array with the visible treasures. It sets specificVisibleTreasures attribute.
+    # @param tHidden [int] Array with the hidden treasures. It sets specificHiddenTreasures attribute.
 
     def initialize(text,levels,death,nVisible,nHidden,tVisible,tHidden)
         @text = text
@@ -52,25 +37,37 @@ class BadConsequence
     ##########
     ########## new_* methods need to review default values
     ##########
+
+    # -------------------- Public constructors -------------------- #
+
+    # Constructor that initializes every attribute but the arrays and death.
+
     def self.new_by_number(text, levels, nVisible, nHidden)
         new(text,levels,false,nVisible,nHidden,nil,nil)
     end 
+
+    # Constructor that initializes the attributes text and death.
 
     def self.new_by_death(text,death)
         new(text,0,death,0,0,nil,nil)
         
     end
 
+    # Constructor that initializes the attributes text, levels and both treasures array.
+
     def self.new_by_specific(text, levels, tVisible, tHidden)
         new(text,levels, false, 0, 0, tVisible, tHidden)
     end
     
+    # Get Methods available for attributes text, levels, nVisibleTreasures, nHiddenTreasures and death.
     attr_reader :text, :levels, :nVisibleTreasures, :nHiddenTreasures, :death
-    #esto o getText, getLevels, etc.?
 
-    #Anula el método constructor new para que no podamos usarlo tal cual  
+
+    #Disables public constructor new in order to guarantee a right initialization of a BadConsequence instance. 
     private_class_method :new
 
+    # Returns a string with the Bad Consequence instance contents.
+    # @return String with the contents.
     def toString()
           return "Text = " + text.to_s + "\nLevels = " + levels.to_s + "\nnVisibleTreasures = "\
           + nVisibleTreasures.to_s + "\nnHiddenTreasures = " + nHiddenTreasures.to_s + "\nDeath = " + death.to_s
